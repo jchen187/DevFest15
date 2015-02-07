3d6d85f10527447373609a1a2cd29722f12d29b3
Johnny Chen <j_chen187@yahoo.com>
2015-02-06 23:14:10 -0500

    The map should start from your current position

--- TheDonutApp/TheDonutApp/ViewController.h
+++ TheDonutApp/TheDonutApp/ViewController.h
@@ -7,8 +7,9 @@
 //
 
 #import <UIKit/UIKit.h>
+#import <GoogleMaps/GoogleMaps.h>
 
-@interface ViewController : UIViewController
+@interface ViewController : UIViewController<GMSMapViewDelegate>
 
 
 @end
--- TheDonutApp/TheDonutApp/ViewController.m
+++ TheDonutApp/TheDonutApp/ViewController.m
@@ -7,7 +7,6 @@
 //
 
 #import "ViewController.h"
-#import <GoogleMaps/GoogleMaps.h>
 
 @interface ViewController (){
     GMSMapView *mapView_;
@@ -18,23 +17,47 @@
 @implementation ViewController
 
 - (void)viewDidLoad {
-    [super viewDidLoad];
+    //[super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
     //Create a GMSCameraPosition that tells the map to display the coordinate -33.86,151.20 at zoom level 6
-    GMSCameraPosition *camera = [GMSCameraPosition cameraWithLatitude:-33.86
-                                                            longitude:151.20
-                                                                 zoom:6];
+    GMSCameraPosition *camera = [GMSCameraPosition
+                                 cameraWithLatitude:mapView_.myLocation.coordinate.latitude
+                                 longitude:mapView_.myLocation.coordinate.longitude
+                                 zoom:16];
+    
+    
     mapView_ = [GMSMapView mapWithFrame:CGRectZero camera:camera];
+    mapView_.delegate = self;
     mapView_.myLocationEnabled = YES;
+    NSLog(@"User's location: %@", mapView_.myLocation);
+
+    //mapView_.mapType = kGMSTypeSatellite;
     self.view = mapView_;
+    mapView_.settings.compassButton = YES;
+    mapView_.settings.myLocationButton = YES; //if know your location and you press it shows your location
+    
+    
     
     //Create a marker in the center of the map
     GMSMarker *marker = [[GMSMarker alloc] init];
-    marker.position = CLLocationCoordinate2DMake(-33.86, 151.20);
-    marker.title = @"Sydney";
-    marker.snippet = @"Australia";
+    //marker.position = CLLocationCoordinate2DMake(-33.86, 151.20);
+    marker.position = camera.target;
+    marker.appearAnimation = kGMSMarkerAnimationPop;
+    //marker.title = @"Sydney";
+    //marker.snippet = @"Australia";
     marker.map = mapView_;
+}
+
+#pragma mark - GMSMapViewDelegate
+- (void)mapView:(GMSMapView *)mapView didTapAtCoordinate:(CLLocationCoordinate2D)coordinate{
+    NSLog(@"You tapped at %f,%f",coordinate.latitude,coordinate.longitude);
+}
+
+- (void)mapView:(GMSMapView *)mapView willMove:(BOOL)gesture{
+    //gesture = TRUE;
+    //removes the marker
+    //[mapView_ clear];
 }
 
 - (void)didReceiveMemoryWarning {
