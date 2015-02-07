3fe2329d66151818d5a69069a1db83b68493d0ab
Johnny Chen <j_chen187@yahoo.com>
2015-02-06 19:13:32 -0500

    Added the file

---
+++ TheDonutApp/TheDonutApp/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  TheDonutApp
+//
+//  Created by Johnny Chen on 2/6/15.
+//  Copyright (c) 2015 JohnnyChen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ TheDonutApp/TheDonutApp/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  TheDonutApp
+//
+//  Created by Johnny Chen on 2/6/15.
+//  Copyright (c) 2015 JohnnyChen. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
